851c4e96d7eaed191bcd6ea03de61f059803d800
Pratikp328 <prtksmart@gmail.com>
2024-11-16 10:29:53 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+let currentOperand = '';
+let previousOperand = '';
+let operation = undefined;
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    if (number === '0' && currentOperand === '0') return;
+    currentOperand += number;
+    updateDisplay();
+}
+
+function setOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        calculateResult();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function calculateResult() {
+    let result;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = result;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').innerText = currentOperand || '0';
+}
+clearDisplay();
